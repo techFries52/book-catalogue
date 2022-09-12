9845df805558b550c552c3d98719135496ff4c94
jjlrevature <69030071+techFries52@users.noreply.github.com>
2022-09-12 10:27:37 -0500

    Update BookCatalogueApplication.java

--- src/main/java/com/BookCatalogueApplication.java
+++ src/main/java/com/BookCatalogueApplication.java
@@ -54,6 +54,9 @@ public class BookCatalogueApplication {
 //			bookService.saveBook(new Book(null, "On the Shoulders of Titans", "Andrew Rowe", new ArrayList<>(), true, "Arcane Ascension"));
 //			bookService.addGenreToBook("Fantasy", "On the Shoulders of Titans");
 
+			genreService.saveGenre(new Genre(null,"Light Novel"));
+			genreService.saveGenre(new Genre(null,"Non-Fiction"));
+
 			bookService.saveBook(new Book(null,"The Torch that Ignites the Stars", "Andrew Rowe", new ArrayList<>(), false, "Arcane Ascension"));
 			bookService.addGenreToBook("Fantasy", "The Torch that Ignites the Stars");
 
@@ -195,6 +198,212 @@ public class BookCatalogueApplication {
 			bookService.addGenreToBook("Fantasy", "The Broken Kingdoms");
 			bookService.saveBook(new Book(null, "The Kingdom of Gods", "N. K. Jemisin", new ArrayList<>(), false, "The Inheritance Trilogy"));
 			bookService.addGenreToBook("Fantasy", "The Kingdom of Gods");
+
+			bookService.saveBook(new Book(null, "A Silent Voice 1", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 1");
+			bookService.saveBook(new Book(null, "A Silent Voice 2", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 2");
+			bookService.saveBook(new Book(null, "A Silent Voice 3", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 3");
+			bookService.saveBook(new Book(null, "A Silent Voice 4", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 4");
+			bookService.saveBook(new Book(null, "A Silent Voice 5", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 5");
+			bookService.saveBook(new Book(null, "A Silent Voice 6", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 6");
+			bookService.saveBook(new Book(null, "A Silent Voice 7", "Yoshitoki Oima", new ArrayList<>(), true, "A Silent Voice"));
+			bookService.addGenreToBook("Manga", "A Silent Voice 7");
+
+			bookService.saveBook(new Book(null, "Deadman Wonderland 1", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 1");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 2", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 2");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 3", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 3");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 4", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 4");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 5", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 5");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 6", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 6");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 7", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 7");
+			bookService.saveBook(new Book(null, "Deadman Wonderland 8", "Jinsei Kataoka, Kazuma Kondou", new ArrayList<>(), true, "Deadman Wonderland"));
+			bookService.addGenreToBook("Manga", "Deadman Wonderland 8");
+
+			bookService.saveBook(new Book(null, "Nisekoi 1", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 1");
+			bookService.saveBook(new Book(null, "Nisekoi 2", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 2");
+			bookService.saveBook(new Book(null, "Nisekoi 3", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 3");
+			bookService.saveBook(new Book(null, "Nisekoi 4", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 4");
+			bookService.saveBook(new Book(null, "Nisekoi 5", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 5");
+			bookService.saveBook(new Book(null, "Nisekoi 6", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 6");
+			bookService.saveBook(new Book(null, "Nisekoi 7", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 7");
+			bookService.saveBook(new Book(null, "Nisekoi 8", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 8");
+			bookService.saveBook(new Book(null, "Nisekoi 9", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 9");
+			bookService.saveBook(new Book(null, "Nisekoi 10", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 10");
+			bookService.saveBook(new Book(null, "Nisekoi 11", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 11");
+			bookService.saveBook(new Book(null, "Nisekoi 12", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 12");
+			bookService.saveBook(new Book(null, "Nisekoi 13", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 13");
+			bookService.saveBook(new Book(null, "Nisekoi 14", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 14");
+			bookService.saveBook(new Book(null, "Nisekoi 15", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 15");
+			bookService.saveBook(new Book(null, "Nisekoi 16", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 16");
+			bookService.saveBook(new Book(null, "Nisekoi 17", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 17");
+			bookService.saveBook(new Book(null, "Nisekoi 18", "Naoshi Komi", new ArrayList<>(), true, "Nisekoi"));
+			bookService.addGenreToBook("Manga", "Nisekoi 18");
+
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 1", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 1");
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 2", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 2");
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 3", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 3");
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 4", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 4");
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 5", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 5");
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 6", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 6");
+			bookService.saveBook(new Book(null, "Tokyo Ghoul 7", "Sui Ishida", new ArrayList<>(), true, "Tokyo Ghoul"));
+			bookService.addGenreToBook("Manga", "Tokyo Ghoul 7");
+
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 1", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 1");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 2", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 2");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 3", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 3");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 4", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 4");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 5", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 5");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 6", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 6");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 7", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 7");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 8", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 8");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 9", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 9");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 10", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 10");
+			bookService.saveBook(new Book(null, "Is It Wrong to Try to Pick Up Girls in a Dungeon? 11", "Sui Ishida", new ArrayList<>(), true, "Is It Wrong to Try to Pick Up Girls in a Dungeon?"));
+			bookService.addGenreToBook("Light Novel", "Is It Wrong to Try to Pick Up Girls in a Dungeon? 11");
+
+			bookService.saveBook(new Book(null, "Black Clover 1", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 1");
+			bookService.saveBook(new Book(null, "Black Clover 2", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 2");
+			bookService.saveBook(new Book(null, "Black Clover 3", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 3");
+			bookService.saveBook(new Book(null, "Black Clover 4", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 4");
+			bookService.saveBook(new Book(null, "Black Clover 5", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 5");
+			bookService.saveBook(new Book(null, "Black Clover 6", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 6");
+			bookService.saveBook(new Book(null, "Black Clover 7", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 7");
+			bookService.saveBook(new Book(null, "Black Clover 8", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 8");
+			bookService.saveBook(new Book(null, "Black Clover 9", "Yuki Tabata", new ArrayList<>(), true, "Black Clover"));
+			bookService.addGenreToBook("Manga", "Black Clover 9");
+
+			bookService.saveBook(new Book(null, "The Irregular at Magic Highschool 1", "Tsutomu Sato", new ArrayList<>(), true, "The Irregular at Magic Highschool"));
+			bookService.addGenreToBook("Light Novel", "The Irregular at Magic Highschool 1");
+			bookService.saveBook(new Book(null, "The Irregular at Magic Highschool 2", "Tsutomu Sato", new ArrayList<>(), true, "The Irregular at Magic Highschool"));
+			bookService.addGenreToBook("Light Novel", "The Irregular at Magic Highschool 2");
+			bookService.saveBook(new Book(null, "The Irregular at Magic Highschool 3", "Tsutomu Sato", new ArrayList<>(), true, "The Irregular at Magic Highschool"));
+			bookService.addGenreToBook("Light Novel", "The Irregular at Magic Highschool 3");
+			bookService.saveBook(new Book(null, "The Irregular at Magic Highschool 4", "Tsutomu Sato", new ArrayList<>(), true, "The Irregular at Magic Highschool"));
+			bookService.addGenreToBook("Light Novel", "The Irregular at Magic Highschool 4");
+
+			bookService.saveBook(new Book(null, "The Demon Prince of Momochi House 1", "Aya Shouoto", new ArrayList<>(), true, "The Demon Prince of Momochi House"));
+			bookService.addGenreToBook("Manga", "The Demon Prince of Momochi House 1");
+			bookService.saveBook(new Book(null, "The Demon Prince of Momochi House 2", "Aya Shouoto", new ArrayList<>(), true, "The Demon Prince of Momochi House"));
+			bookService.addGenreToBook("Manga", "The Demon Prince of Momochi House 2");
+			bookService.saveBook(new Book(null, "The Demon Prince of Momochi House 3", "Aya Shouoto", new ArrayList<>(), true, "The Demon Prince of Momochi House"));
+			bookService.addGenreToBook("Manga", "The Demon Prince of Momochi House 3");
+			bookService.saveBook(new Book(null, "The Demon Prince of Momochi House 4", "Aya Shouoto", new ArrayList<>(), true, "The Demon Prince of Momochi House"));
+			bookService.addGenreToBook("Manga", "The Demon Prince of Momochi House 4");
+
+			bookService.saveBook(new Book(null, "Soul Eater 1", "Atsushi Ohkubo", new ArrayList<>(), true, "Soul Eater"));
+			bookService.addGenreToBook("Manga", "Soul Eater 1");
+			bookService.saveBook(new Book(null, "Soul Eater 2", "Atsushi Ohkubo", new ArrayList<>(), true, "Soul Eater"));
+			bookService.addGenreToBook("Manga", "Soul Eater 2");
+
+			bookService.saveBook(new Book(null, "The Seven Deadly Sins 1", "Nakaba Suzuki", new ArrayList<>(), true, "The Seven Deadly Sins"));
+			bookService.addGenreToBook("Manga", "The Seven Deadly Sins 1");
+			bookService.saveBook(new Book(null, "The Seven Deadly Sins 2", "Nakaba Suzuki", new ArrayList<>(), true, "The Seven Deadly Sins"));
+			bookService.addGenreToBook("Manga", "The Seven Deadly Sins 2");
+			bookService.saveBook(new Book(null, "The Seven Deadly Sins 3", "Nakaba Suzuki", new ArrayList<>(), true, "The Seven Deadly Sins"));
+			bookService.addGenreToBook("Manga", "The Seven Deadly Sins 3");
+			bookService.saveBook(new Book(null, "The Seven Deadly Sins 4", "Nakaba Suzuki", new ArrayList<>(), true, "The Seven Deadly Sins"));
+			bookService.addGenreToBook("Manga", "The Seven Deadly Sins 4");
+			bookService.saveBook(new Book(null, "The Seven Deadly Sins 5", "Nakaba Suzuki", new ArrayList<>(), true, "The Seven Deadly Sins"));
+			bookService.addGenreToBook("Manga", "The Seven Deadly Sins 5");
+			bookService.saveBook(new Book(null, "The Seven Deadly Sins 6", "Nakaba Suzuki", new ArrayList<>(), true, "The Seven Deadly Sins"));
+			bookService.addGenreToBook("Manga", "The Seven Deadly Sins 6");
+
+			bookService.saveBook(new Book(null, "Black Lagoon 1", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 1");
+			bookService.saveBook(new Book(null, "Black Lagoon 2", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 2");
+			bookService.saveBook(new Book(null, "Black Lagoon 4", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 4");
+			bookService.saveBook(new Book(null, "Black Lagoon 5", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 5");
+			bookService.saveBook(new Book(null, "Black Lagoon 7", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 7");
+			bookService.saveBook(new Book(null, "Black Lagoon 8", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 8");
+			bookService.saveBook(new Book(null, "Black Lagoon 10", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 10");
+			bookService.saveBook(new Book(null, "Black Lagoon 11", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 11");
+			bookService.saveBook(new Book(null, "Black Lagoon 12", "Rei Hiroe", new ArrayList<>(), true, "Black Lagoon"));
+			bookService.addGenreToBook("Manga", "Black Lagoon 12");
+
+			bookService.saveBook(new Book(null, "Unmasking Autism", "Devon Price", new ArrayList<>(), false, null));
+			bookService.addGenreToBook("Non-Fiction", "Unmasking Autism");
+
+			bookService.saveBook(new Book(null, "The Power of Neurodiversity: Unleashing the Advantages of Your Differently Wired Brain", "Thomas Armstrong", new ArrayList<>(), false, null));
+			bookService.addGenreToBook("Non-Fiction", "The Power of Neurodiversity: Unleashing the Advantages of Your Differently Wired Brain");
+
+			bookService.saveBook(new Book(null, "The Color of Law", "Richard Rothstein", new ArrayList<>(), false, null));
+			bookService.addGenreToBook("Non-Fiction", "The Color of Law");
+
+			bookService.saveBook(new Book(null, "Therapy Game 1", "Meguru Hinohara", new ArrayList<>(), true, "Therapy Game"));
+			bookService.addGenreToBook("Manga", "Therapy Game 1");
+			bookService.saveBook(new Book(null, "Therapy Game 2", "Meguru Hinohara", new ArrayList<>(), true, "Therapy Game"));
+			bookService.addGenreToBook("Manga", "Therapy Game 2");
+
+			bookService.saveBook(new Book(null, "Thigh High Reiwa Hanamaru Academy 1", "Kotobuki", new ArrayList<>(), true, "Thigh High Reiwa Hanamaru Academy"));
+			bookService.addGenreToBook("Manga", "Thigh High Reiwa Hanamaru Academy 1");
+			bookService.saveBook(new Book(null, "Thigh High Reiwa Hanamaru Academy 2", "Kotobuki", new ArrayList<>(), true, "Thigh High Reiwa Hanamaru Academy"));
+			bookService.addGenreToBook("Manga", "Thigh High Reiwa Hanamaru Academy 2");
+			bookService.saveBook(new Book(null, "Thigh High Reiwa Hanamaru Academy 3", "Kotobuki", new ArrayList<>(), true, "Thigh High Reiwa Hanamaru Academy"));
+			bookService.addGenreToBook("Manga", "Thigh High Reiwa Hanamaru Academy 3");
+
+			bookService.saveBook(new Book(null, "Frieren 1", "Kotobuki", new ArrayList<>(), true, "Frieren"));
+			bookService.addGenreToBook("Manga", "Frieren 1");
+
 		};
 	}
 }
